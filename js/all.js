33ccdd886c64cd7f2b807da9c5630968b785cf48
roby <robyhung@gmail.com>
2020-02-08 22:06:34 +0800

    修改地圖紅色標記無法顯示的問題

--- js/all.js
+++ js/all.js
@@ -43,7 +43,7 @@ function geoFindMe() {
 
 
     var redIcon = new L.Icon({
-      iconUrl: '../images/marker-icon-red.png',
+      iconUrl: './images/marker-icon-red.png',
       shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
       iconSize: [25, 41],
       iconAnchor: [12, 41],
